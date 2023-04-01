cf4851f143e85ba8f42c62c2991d6005ef5f8a6f
Lira11io <Lira11pm@mail.ru>
2023-04-02 02:05:15 +0300

    add html, js, assets

---
+++ index.js
@@ -0,0 +1,16 @@
+/* let value = input.setAttribute("value", "До свидания");
+console.log(input.hasAttribute("value")); */
+/* const tableAge = document.getElementById("age-table");/*найдет таблицу с id=age-table*/
+/*console.log(tableAge); */
+/* let elements =
+  document.getElementsByTagName(
+    "label"
+  );  */ /*Это если хотим найти все такие элементы во всем коде*/
+/* console.log(elements);
+let label = document.querySelectorAll("label"); */ /*Это если хотим найти все такие элементы во всем коде*/
+/* console.log(label); */
+/* let labels = tableAge.getElementsByTagName("label"); */ /*найдет элемент label по имени в таблице*/
+/* console.log(labels); */
+/* const form = document.getElementsByName("search-person"); */ /*найдет элемент form с именем search-person*/
+/* console.log(form);
+ */
