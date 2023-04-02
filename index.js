ad406626d6facfc099694c523909591877f5a638
Lira11io <Lira11pm@mail.ru>
2023-04-02 22:42:27 +0300

    добавлено решение практического задания и добавлены стили для страницы

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ let label = document.querySelectorAll("label"); */ /*Это если хотим 
 /* const form = document.getElementsByName("search-person"); */ /*найдет элемент form с именем search-person*/
 /* console.log(form);
  */
+/* const tableAge1 = document.querySelector("#age-table");
+console.log(tableAge1);
+ */
+
+/*Практическое задание*/
+const price1 = document.querySelector("#price1").innerHTML;
+const price2 = document.querySelector("#price2").innerHTML;
+const price3 = document.querySelector("#price3").innerHTML;
+const price4 = document.querySelector("#price4").innerHTML;
+
+const sum =
+  Number.parseInt(price1) +
+  Number.parseInt(price2) +
+  Number.parseInt(price3) +
+  Number.parseInt(price4);
+
+const total = document.querySelector("#total");
+
+total.textContent = sum;
+
+function resultTotal() {
+  total.textContent = sum - (sum * 20) / 100;
+}
